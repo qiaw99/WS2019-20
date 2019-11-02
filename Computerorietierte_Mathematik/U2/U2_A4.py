fd61243bc84160e4558da9b9e56f633abf061ecd
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-11-02 13:30:15 +0100

    Update U2_A4.py

--- Computerorietierte_Mathematik/U2/U2_A4.py
+++ Computerorietierte_Mathematik/U2/U2_A4.py
@@ -25,40 +25,40 @@ def plotGraphic():
 	plt.show()
 
 	# Aufgabe 4 b)
-	x = np.arange(0,11,0.1)
+	x = np.arange(0, 11, 0.1)
 	y = np.floor(x)
 	plt.title("Plot der floorfunktion")
 	plt.xlabel("x axis caption")
 	plt.ylabel("y axis caption")
-	plt.plot(x,y,"ob")
+	plt.plot(x, y, "ob")
 	plt.savefig("plot2.png")
 	plt.show()
 
 	# Aufgabe 4 c)
-	x = np.arange(0,11)
+	x = np.arange(0, 11)
 	y = x * x
 	plt.title("Plot der Funktion mit skalierten Achsen")
 	plt.xlabel("x axis caption")
 	plt.ylabel("y axis caption")
-	plt.loglog(x,y,basex = 2, basey = 2)
+	plt.loglog(x, y, basex = 2, basey = 2)
 	plt.savefig("plot3.png")
 	plt.show()
 
 
 	# Aufgabe 4 d)
-	x = np.arange(0,2 * math.pi + 1, 0.01)
+	x = np.arange(0, 2 * math.pi + 1, 0.01)
 	y1 = np.sin(x)
 
 	plt.title("Plot der Funktion mit skalierten Achsen", ha = "left")
 
-	plt.subplot(1,1,1)
-	plt.plot(x,y1,color = 'r')
+	plt.subplot(1, 1, 1)
+	plt.plot(x, y1, color = 'r')
 
 	y2 = np.cos(x)
-	plt.plot(x,y2,'b') 
+	plt.plot(x, y2, 'b') 
 
-	y3 = 1/np.square(math.pi) * x * (2*math.pi - x)
-	plt.plot(x,y3,'y')
+	y3 = 1 / np.square(math.pi) * x * (2 * math.pi - x)
+	plt.plot(x, y3, 'y')
 	plt.savefig("plot4.png")
 	plt.show()
 
