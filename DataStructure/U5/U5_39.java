163b613d30cc53f610d8a0f50a6a108347def07f
Nazar Sopiha <nazar.sopiha@gmail.com>
2019-11-13 11:00:16 +0100

    ready

--- DataStructure/U5/U5_39.java
+++ DataStructure/U5/U5_39.java
@@ -1,85 +1,85 @@
+import java.util.*;
 class TreeNode{
-	public int key;
-	public TreeNode left, right, parent;
-	
-	public TreeNode(int key){
-		this.key = key;
-	}
+        public int key;
+        public TreeNode left, right, parent;
+
+        public TreeNode(int key){
+                this.key = key;
+        }
 }
 
 class BinarySearchTree{
-	public TreeNode root;
-	public int nodes;
-	
-	public BinarySearchTree(){
-		root = null;
-		nodes = 0;
-	}
+        public TreeNode root;
+        public int nodes;
+        public static ArrayList<TreeNode> nodeList;
 
-	public int getTotal(){
-		int total = 0;
-		return (getInternalLength(root.left) + getInternalLength(root) + getInternalLength(root.right)) / this.getSize();
-	}
+        public BinarySearchTree(){
+                root = null;
+                nodes = 0;
+                nodeList = new ArrayList<TreeNode>();
+        }
 
-	public int getSize(){
-		return this.nodes;
-	}
+        public float getTotal(){
+                float total = 0;
+                for (TreeNode i : nodeList) {
+                    total += getInternalLength(i);
+                }
+                return total / this.getSize();
 
-	public int getInternalLength(TreeNode n){
-		int counter = 0;
-		if(n == root)
-			return counter;
-		while(n != root){
-			counter ++;
-			n = n.parent;
-		}
-		return counter;
-	}
-	
-	public void inOrder(TreeNode x) {
-		if(x != null) {
-			inOrder(x.left);
-			System.out.print(x.key + " ");
-			inOrder(x.right);
-		}
-	}
+        }
 
-	public void insert(BinarySearchTree T, TreeNode z) {
-		nodes ++;
-		TreeNode y = null;
-		TreeNode x = T.root;
-		while(x != null) {
-			y = x;
-			if(z.key < x.key) {
-				x = x.left;
-			}else {
-				x = x.right;
-			}
-		}
-		z.parent = y;
-		if(y == null) {
-			T.root = z;
-		}else if(z.key < y.key) {
-			y.left = z;
-		}else {
-			y.right = z;
-		}
-	}
+        public int getSize(){
+                return this.nodes;
+        }
+
+        public int getInternalLength(TreeNode n){
+                int counter = 0;
+                if(n == root)
+                        return counter;
+                while(n != root){
+                        counter ++;
+                        System.out.println("Key of element is: " + n.key);
+                        n = n.parent;
+                }
+                return counter;
+        }
+
+        public void insert(BinarySearchTree T, TreeNode z) {
+                nodes ++;
+                TreeNode y = null;
+                TreeNode x = T.root;
+                while(x != null) {
+                        y = x;
+                        if(z.key < x.key) {
+                                x = x.left;
+                        }else {
+                                x = x.right;
+                        }
+                }
+                z.parent = y;
+                if(y == null) {
+                        T.root = z;
+                }else if(z.key < y.key) {
+                        y.left = z;
+                }else {
+                        y.right = z;
+                }
+                nodeList.add(z);
+        }
 }
 
 public class U5_39{
-	public static void main(String args[]){
-		int[] arr = new int []{5,4,3,2,1};
-		BinarySearchTree tree = transform(arr);
-		tree.inOrder(tree.root);
-		System.out.println(tree.getTotal());
-	}
+        public static void main(String args[]){
+                int[] arr = new int []{10,12,4,8,1};
+                BinarySearchTree tree = transform(arr);
+                System.out.println(tree.getTotal());
+        }
 
-	public static BinarySearchTree transform(int [] arr){
-		BinarySearchTree tree = new BinarySearchTree();
-		for(int i = 0; i < arr.length; i++){
-			tree.insert(tree, new TreeNode(arr[i]));
-		}
-		return tree;
-	}
+        public static BinarySearchTree transform(int [] arr){
+                BinarySearchTree tree = new BinarySearchTree();
+                for(int i = 0; i < arr.length; i++){
+                        tree.insert(tree, new TreeNode(arr[i]));
+                }
+                return tree;
+        }
 }
