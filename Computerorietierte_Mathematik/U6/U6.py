41c32f7772609f49657d51b83d25b322e11e003d
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-12-01 11:41:03 +0100

    Update U6.py

--- Computerorietierte_Mathematik/U6/U6.py
+++ Computerorietierte_Mathematik/U6/U6.py
@@ -1,35 +1,22 @@
 import math
+from tabulate import tabulate
 
-def concat(f,g,x):
-	return f1(f2(x))
+f1 = lambda x : x * (x + 2)
+f2 = lambda x : math.sqrt(x + 1) - 1
 
-def f1(x):
-	if(x >= -1):
-		y = x * (x + 2)
-		if(y >= -1):
-			return y
-		else:
-			raise Exception("y is out of range!")
-	else:
-		raise Exception("x is out of range!")
-
-def f2(x):
-	if(x >= -1):
-		y = math.sqrt(x + 1) - 1
-		if(y >= -1):
-			return y
-		else:
-			Exception("y is out of range!")
-	else:
-		raise Exception("x is out of range!")
+def concat(f1,f2):
+	return lambda x : f1(f2(x))
 
 def generateData(x):
-	val1 = concat(f2,f1,x)
+	h = concat(f2,f1)	# f^(-1) * f
+	g = concat(f1,f2)
+	val1 = h(x)
 	err1 = abs(val1 - x)
-	val2 = concat(f1,f2,x)
+	val2 = g(x)
 	err2 = abs(val2 - x)
-	#return (val1, val2, err1, err2)
-	return val1 == val2
+	return (val1, val2, err1, err2)
 
+temp = []
 for i in range(13):
-	print(generateData(-1 + 10**(-i)))
+	ls = list(generateData(-1 + 10**(-i)))
+	tmep.append(ls)
