9f44fbc51b0fc4689059969cf215a376e5c16f0d
Nazar Sopiha <nazar.sopiha@gmail.com>
2019-12-11 10:26:14 +0100

    59 task ALP3. Small hash table

---
+++ DataStructure/U9/U9_59.py
@@ -0,0 +1,18 @@
+#Permutation für t = 1, 3, 5, 13
+
+def hashfunction():
+    t_values = [0, 1, 2, 3, 4, 5, 13, 20]
+    res = {}
+    for t in t_values:
+        temp = [0 for i in range(1024)]
+        for i in range(1024):
+            temp[ (t*i) % 1024] += 1
+        m = max(temp)
+        print(m)
+        res[t] = m
+    print(res)
+    return res
+
+
+hashfunction()
+print("Permutation für t = 1, 3, 5, 13")
