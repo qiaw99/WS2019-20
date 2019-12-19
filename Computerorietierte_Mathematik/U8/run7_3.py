9229df70a0ee54ff2c5b147b5a458baff0855283
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-12-19 15:36:16 +0100

    Update run7_3.py

--- Computerorietierte_Mathematik/U8/run7_3.py
+++ Computerorietierte_Mathematik/U8/run7_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 def ggT_tumb(a, b):
@@ -33,17 +33,60 @@ def ggT_euclid(a, b):
         n = r
     return (m, counter)
 
-def main():
+def draw_graph(func):
     # {100,...,1000}
     a = np.random.randint(900, size = 1000) + 100
     b = np.random.randint(900, size = 1000) + 100
     ls = []
-    counter = 0
     for i in range(1000):
-        counter = ggT_tumb(a[i], b[i])[1]
+        counter = func(a[i], b[i])[1]
         ls.append(counter)
+    gr1 = 0
+    gr2 = 0
+    gr3 = 0
+    gr4 = 0
+    gr5 = 0
+    gr6 = 0
+    gr7 = 0
+    gr8 = 0
+    gr9 = 0
+    gr10 = 0
+    maximum = max(ls)
+    minimum = min(ls)
 
-    print(ls)
+    for x in ls:
+        if x < maximum/10:
+            gr1 += 1
+        elif x < 2*maximum/10:
+            gr2 += 1
+        elif x < 3*maximum/10:
+            gr3 += 1
+        elif x < 4*maximum/10:
+            gr4 += 1
+        elif x < 5*maximum/10:
+            gr5 += 1
+        elif x < 6*maximum/10:
+            gr6 += 1
+        elif x < 7*maximum/10:
+            gr7 += 1
+        elif x < 8*maximum/10:
+            gr8 += 1
+        elif x < 9*maximum/10:
+            gr9 += 1
+        else:
+            gr10 += 1
+    y = [gr1, gr2, gr3, gr4, gr5,gr6, gr7, gr8, gr9, gr10]
+    x = ['>' + str(i) for i in range (0, maximum+1, maximum//10)]
+    plt.title("ggT_tumb; max is " + str(maximum) + " min is " + str(minimum))
+    plt.xlabel("Gruppe nach der Anzahl der Vergleiche")
+    plt.ylabel("Anzahl der Elemente in der Gruppe")
+    plt.bar(range(len(y)), y, fc = 'r', tick_label = x)
+    plt.show()
+
+def main():
+    draw_graph(ggT_tumb)
+    draw_graph(ggT_tumbpp)
+    draw_graph(ggT_euclid)
 
 if __name__ == "__main__":
     main()
