70143bcf0de79052dbd9970803128c3367433084
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-01-08 11:18:27 +0100

    Update U11_76.java

--- DataStructure/U11/U11_76.java
+++ DataStructure/U11/U11_76.java
@@ -1,32 +1,78 @@
-package Test;
-
 @SuppressWarnings("serial")
-class ResultFounded extends Exception{}
+class ResultFound extends Exception{}
 
 class Drachen{
 	int n;	//n Drachen auf einem nxn Schachbrett
 	int [][] res;
+	boolean [][] capture;
 	long counter;
 	
 	public Drachen(int n) {
 		this.n = n;
 		this.counter = 0;
 		res = new int[n][n];
+		capture = new boolean[n][n];
+		initialize();
+		
 		try {
 			put(0);
-		}catch(ResultFounded e){
+		}catch(ResultFound e){
 			
 		}
 	}
 	
-	public void put(int x) throws ResultFounded{
+	private void initialize() {
+		for(int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				res[i][j] = 0;
+				capture[i][j] = false;
+			}
+		}
+	}
+	
+	public void add(int i, int j) {
+		for(int a = 0; a < n; a++) {
+			// Spalte checken
+			capture[a][j] = true;
+			// Zeile checken
+			capture[i][a] = true;
+		}
+		// 1 Diag
+		int a = i,b = j;																																																																																																																																																																																				
+		while(a < n && b < n) {
+			capture[++a][++b] = true;
+		}
+		
+		// 2 Diag
+		a = i;
+		b = j;																																																																																																																																																																																				
+		while(a < n && b < n) {
+			capture[++a][++b] = true;
+		}
+		
+		a = i;b = j;																																																																																																																																																																																				
+		while(a < n && b < n) {
+			capture[++a][++b] = true;
+		}
+		
+		a = i;b = j;																																																																																																																																																																																				
+		while(a < n && b < n) {
+			capture[++a][++b] = true;
+		}
+		
+	}
+	
+	public void put(int x) throws ResultFound{
+		// TODO
 		if(x == n) {
-			throw new ResultFounded();
+			throw new ResultFound();
 		}else {
 			for(int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
 					if(!conflict(i, j, x)) {
 						res[i][j] = 1;
+						capture[i][j] = true;
+						add(i, j);
 						counter ++;
 						put(x + 1);
 					}
@@ -37,21 +83,20 @@ class Drachen{
 
 	public boolean conflict(int i, int j, int k) {
 		int sum = 0;
-		int temp = 0;
+		int capture = 0;
 		for(int a = 0; a < n; a++) {
 			// check the raw
 			for(int b = 0; b < n; b++) {
 				sum += res[a][b];
 				// check the column
-				temp += res[b][a];
+				capture += res[b][a];
 			}
-			if(sum > 1 || temp > 1) {
+			if(sum > 1 || capture > 1) {
 				return true;
 			}else {
 				sum = 0;
 			}
 		}
-		
 		return false;
 	}
 	
