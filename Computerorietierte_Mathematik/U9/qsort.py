091d08155c43c705c2f51b985b5503dc8c55d995
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-01-12 13:05:21 +0100

    Update qsort.py

--- Computerorietierte_Mathematik/U9/qsort.py
+++ Computerorietierte_Mathematik/U9/qsort.py
@@ -5,6 +5,8 @@ __copyright__ = "Copyright (c) 2019 qiaw99"
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Aufgabe 3
@@ -35,4 +37,44 @@ def private_quicksort(A):
     
     return (quicksort(A, 0, len(A) - 1), counter)
     
-print(private_quicksort([1,3,2,4,5]))
+def main():
+    for k in range(1, 5):
+        ls = []
+        #lists which store counters for each sort-algorithms
+        helper1 = []
+
+        maximum1 = []
+        minimum1 = []
+        durchschnitt1 = []
+        for i in range(100):
+            temp = [random.random() for j in range(10**k)]
+            ls.append(temp)
+            
+            ######
+            a, b = private_quicksort(temp)
+            helper1.append(b / 10**k)
+            
+            maximum1.append(max(helper1))
+            minimum1.append(min(helper1))
+            durchschnitt1.append(sum(helper1) / len(helper1))
+           
+
+        print("Waiting for the result... with length ", 10**k)
+        print(len(maximum1))
+        x1 = np.arange(0, 100)    
+        y1 = np.array(maximum1)
+
+        plt.title("Quicksort")
+        #plt.subplot(1, 1, 1)
+        plt.plot(x1, y1, 'r', label = "maximum")
+        
+        y2 = np.array(minimum1)
+        plt.plot(x1, y2, 'b', label = "minimum") 
+        
+        y3 = np.array(durchschnitt1)
+        plt.plot(x1, y3, 'g', label = "durchschnitt") 
+        plt.legend(loc = 'upper right')
+        plt.show()
+
+if __name__ == '__main__':
+    main()
