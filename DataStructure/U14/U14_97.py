cef4cbf6e4f2ebd048a684151b257f08dd45b304
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-02-06 13:10:19 +0100

    Update U14_97.py

--- DataStructure/U14/U14_97.py
+++ DataStructure/U14/U14_97.py
@@ -1,25 +1,11 @@
-money = 143
-coins = [1, 2, 5, 10, 20, 50]
-res = []
-memo = [[None] for _ in range(money + 1)]
-memo[1] = [1]
-memo[2] = [2]
-memo[5] = [5]
-memo[10] = [10]
-memo[20] = [20]
-counter = 0
-
-def changeMoney(x):
-    global coins, res, memo, counter
-    if(memo[x] != [None]):
-        res.extend(memo[x])
-        return 
-    for temp in reversed(coins):
-        if temp <= x:
-            res.append(temp)
-            counter += 1
-            return changeMoney(x-temp)
-
-changeMoney(money)
-print(res)
-print(counter)
+nums = 34
+coins= [1,2,5,10]
+temp = nums
+dp = [100 for _ in range(nums)]
+dp[0]=0
+for i in range(1,nums):
+    for j in range(len(coins)):
+        if(coins[j] <= nums):
+            dp[i] = min(dp[i], dp[i-coins[j]]+1)
+print(dp)            
+print(dp[nums-1])
